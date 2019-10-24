9ed8de3f6d80e3a66548d0ff783ee0a0b239ea9a
mamadou sylla <m27sylla@gmail.com>
2019-10-24 14:04:08 +0200

    Updates

---
+++ precache-manifest.506277d956e47d5a34512b0b2b5c0709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ce6766921efed2eae56ba58d77402b",
+    "url": "/react-flats-search/index.html"
+  },
+  {
+    "revision": "93b2c6f9f2e498d670da",
+    "url": "/react-flats-search/static/css/main.755870d9.chunk.css"
+  },
+  {
+    "revision": "2bd5de0d2a667f73d171",
+    "url": "/react-flats-search/static/js/2.9fcf7012.chunk.js"
+  },
+  {
+    "revision": "93b2c6f9f2e498d670da",
+    "url": "/react-flats-search/static/js/main.4add29b8.chunk.js"
+  },
+  {
+    "revision": "ee38471b1e2cfd540391",
+    "url": "/react-flats-search/static/js/runtime-main.4201e137.js"
+  }
+]);
